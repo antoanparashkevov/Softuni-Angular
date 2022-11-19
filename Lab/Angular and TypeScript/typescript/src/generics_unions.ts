15d202383b3f19be1987e04f413fd4b62ff4e9e2
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-19 16:32:13 +0200

    Typescript Unions

---
+++ Lab/Angular and TypeScript/typescript/src/generics_unions.ts
@@ -0,0 +1,7 @@
+//Unions
+type myBool = true | false
+
+type WindowStates = 'open' | 'closed' | 'minimized'
+
+// const window: WindowStates = 'asd'//we can't
+const window: WindowStates = 'open'//or closed or minimized
