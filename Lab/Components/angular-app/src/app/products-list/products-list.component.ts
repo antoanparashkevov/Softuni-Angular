91e30e3407df8112ce51fb809dcd3be6c25e07a2
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-20 17:09:21 +0200

    String interpolation (TScript to Template), Event Binding (Template to TScrypt)

--- Lab/Components/angular-app/src/app/products-list/products-list.component.ts
+++ Lab/Components/angular-app/src/app/products-list/products-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {EventType} from "@angular/router";
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent {
+  title: string = 'Welcome to product-list component'
 
-  constructor() { }
+  changeTitle: boolean | undefined = true
 
-  ngOnInit(): void {
+  constructor() {
+    if(this.changeTitle) {
+      this.title = 'Welcome again, guest :)'
+    }
+
+  }
+
+  clickChangeTitle(ev: MouseEvent) {
+    console.log(ev)
   }
 
 }
